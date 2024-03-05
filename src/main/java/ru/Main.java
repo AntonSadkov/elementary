f956183dbbeb00085e1d68a5569c9b0da1f04209
AntonSadkov <antonsadkov96@gmail.com>
2024-03-05 21:13:42 +0300

    Clean code

--- src/main/java/ru/Main.java
+++ src/main/java/ru/Main.java
@@ -1,17 +1,7 @@
 package ru;
 
-import ru.calculator.Calculator;
-
 public class Main {
     public static void main(String[] args) {
-        //System.out.println("Hello world");
-
-        int size = 10;
-        size = 100;
-        size -= 5;
-
-        System.out.println(size);
-
-        
+        System.out.println("Hello world");
     }
 }
