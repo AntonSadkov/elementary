ffdada0983cfc247d8b0d462e2135ed7df6f9fee
AntonSadkov <antonsadkov96@gmail.com>
2024-03-06 23:14:02 +0300

    1.11 Расстояние между точками в системе координат.

---
+++ src/main/java/ru/condition/Point.java
@@ -0,0 +1,13 @@
+package ru.condition;
+
+public class Point {
+    public static double distance(int x1, int y1, int x2, int y2) {
+        double result = Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2);
+        return Math.sqrt(result);
+    }
+
+    public static void main(String[] args) {
+        double result = distance(1, 2, 3, 4);
+        System.out.printf("Result (1, 2) to (2, 0) %.4f", result);
+    }
+}
