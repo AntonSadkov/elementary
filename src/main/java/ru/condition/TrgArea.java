9811e5c37cdd24a8b841d4a7a71a92f5c407cd42
AntonSadkov <antonsadkov96@gmail.com>
2024-03-09 16:54:00 +0300

    1.13 Площадь треугольника.

--- src/main/java/ru/condition/TrgArea.java
+++ src/main/java/ru/condition/TrgArea.java
@@ -3,10 +3,11 @@ package ru.condition;
 public class TrgArea {
 
     public static double area(double a, double b, double c) {
-        return -1;
+        double p = (a + b + c) / 2;
+        return Math.sqrt(p * (p - a) * (p - b) * (p -c));
     }
 
     public static void main(String[] args) {
-
+        System.out.printf("area (2, 3, 4) = %.3f", area(2, 3, 4));
     }
 }
