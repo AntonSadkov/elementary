77b59dd47df70d3d37415a661cf54d70197bc41b
AntonSadkov <antonsadkov96@gmail.com>
2024-03-09 16:46:08 +0300

    1.12 Стороны прямоугольника.

---
+++ src/main/java/ru/condition/TrgArea.java
@@ -0,0 +1,12 @@
+package ru.condition;
+
+public class TrgArea {
+
+    public static double area(double a, double b, double c) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
