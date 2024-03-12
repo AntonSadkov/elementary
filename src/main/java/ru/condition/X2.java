c62ff5a9fd98fa18540a931ba90d40e7287dd196
AntonSadkov <antonsadkov96@gmail.com>
2024-03-12 23:05:51 +0300

    2.3 Входные данные в тесте.Adding main.

--- src/main/java/ru/condition/X2.java
+++ src/main/java/ru/condition/X2.java
@@ -4,4 +4,12 @@ public class X2 {
     public static int calc(int a, int b, int c, int x) {
         return a * x * x + b * x + c;
     }
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 0;
+        int c = 0;
+        int x = 2;
+        System.out.println(calc(a, b, c, x));
+    }
 }
