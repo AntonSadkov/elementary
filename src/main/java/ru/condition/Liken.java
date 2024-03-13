9cdc9098b51ba946f219bdd90cfa1e2c32d6d244
AntonSadkov <antonsadkov96@gmail.com>
2024-03-13 20:38:10 +0300

    3 Операции сравнения.

---
+++ src/main/java/ru/condition/Liken.java
@@ -0,0 +1,14 @@
+package ru.condition;
+
+public class Liken {
+    public static void main(String[] args) {
+        int first = 10;
+        int second = 9;
+
+        System.out.println(first > second);
+
+        System.out.println(first < second);
+
+        System.out.println(first == second);
+    }
+}
