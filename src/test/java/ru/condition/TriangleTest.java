9593f7621f991fd796d65bf8358defca7890b4d5
AntonSadkov <antonsadkov96@gmail.com>
2024-03-15 20:07:25 +0300

    3.13 Множественное логическое выражение.

---
+++ src/test/java/ru/condition/TriangleTest.java
@@ -0,0 +1,20 @@
+package ru.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class TriangleTest {
+
+    @Test
+    void whenExist() {
+        assertThat(Triangle.exist(3,2,2))
+                .isTrue();
+    }
+
+    @Test
+    void whenNotExist() {
+        assertThat(Triangle.exist(3,1,2))
+                .isFalse();
+    }
+}
