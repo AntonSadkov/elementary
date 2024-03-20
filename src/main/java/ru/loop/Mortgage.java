c4358e600d524988eed0f8d2af2dc66f71dceac6
AntonSadkov <antonsadkov96@gmail.com>
2024-03-20 12:36:07 +0300

    4.10 Ипотека.Fixes.

--- src/main/java/ru/loop/Mortgage.java
+++ src/main/java/ru/loop/Mortgage.java
@@ -4,7 +4,6 @@ public class Mortgage {
     public static int calculatingLoanTerm(double amount, double salary, double percent) {
         int year = 0;
         percent /= 100;
-
         while (amount > 0) {
             amount += amount * percent - salary;
             year++;
