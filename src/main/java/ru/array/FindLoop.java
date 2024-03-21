ea30316c1dca81b43be6184c4869e75d3104a28f
AntonSadkov <antonsadkov96@gmail.com>
2024-03-22 00:35:09 +0300

    5.8 Классический поиск перебором.Fixes

--- src/main/java/ru/array/FindLoop.java
+++ src/main/java/ru/array/FindLoop.java
@@ -2,8 +2,11 @@ package ru.array;
 
 public class FindLoop {
     public static int indexOf(int[] data, int el) {
-        for (int i = 0; i < data.length; i++)
-            if (data[i] == el) return i;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == el) {
+                return i;
+            }
+        }
         return -1;
     }
 }
