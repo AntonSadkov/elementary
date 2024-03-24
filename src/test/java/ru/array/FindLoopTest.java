251457b03664d0d83ca1c839d2646b36bae18508
AntonSadkov <antonsadkov96@gmail.com>
2024-03-24 13:03:28 +0300

    5.18 Поиск индекса в диапазоне.

--- src/test/java/ru/array/FindLoopTest.java
+++ src/test/java/ru/array/FindLoopTest.java
@@ -21,4 +21,36 @@ class FindLoopTest {
         assertThat(FindLoop.indexOf(data, 5))
                 .isEqualTo(expected);
     }
+
+    @Test
+    void whenDiapasonHas8ThenResultEqualFinish() {
+        int[] data = {5, 2, 10, 2, 4, 8, 14, 3, 21, 16};
+        int expected = 5;
+        assertThat(FindLoop.indexInRange(data, 8, 2, 5))
+                .isEqualTo(expected);
+    }
+
+    @Test
+    void whenDiapasonHasSomeEqualElementThen3() {
+        int[] data = {5, 10, 2, 4, 8, 14, 3, 21, 16};
+        int expected = 3;
+        assertThat(FindLoop.indexInRange(data, 4, 1, 8))
+                .isEqualTo(expected);
+    }
+
+    @Test
+    void whenDiapasonHas2Then3() {
+        int[] data = {5, 2, 10, 2, 4};
+        int expected = 3;
+        assertThat(FindLoop.indexInRange(data, 2, 2, 4))
+                .isEqualTo(expected);
+    }
+
+    @Test
+    void whenDiapasonHasNot2ThenMinus1() {
+        int[] data = {5, 1, 10, 1, 4};
+        int expected = -1;
+        assertThat(FindLoop.indexInRange(data, 2, 0, 4))
+                .isEqualTo(expected);
+    }
 }
