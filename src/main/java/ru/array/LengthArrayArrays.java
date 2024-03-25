311fdf62095b67c66731629f34baa8b830b2fc52
AntonSadkov <antonsadkov96@gmail.com>
2024-03-25 09:15:52 +0300

    5.23 Размер элемента двухмерного массива.

---
+++ src/main/java/ru/array/LengthArrayArrays.java
@@ -0,0 +1,10 @@
+package ru.array;
+
+public class LengthArrayArrays {
+    public static void main(String[] args) {
+        int[][] numbers = {{1}, {4, 5}, {7, 8, 9}, {10, 11, 12, 13}};
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i].length);
+        }
+    }
+}
