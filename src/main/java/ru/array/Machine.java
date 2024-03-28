f4cfc64b28e0ecc763924aeffb363808cb629393
AntonSadkov <antonsadkov96@gmail.com>
2024-03-28 13:19:17 +0300

    5.32 Жадный алгоритм.Сдача в кофе машине. Fixes.

--- src/main/java/ru/array/Machine.java
+++ src/main/java/ru/array/Machine.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Machine {
     public static int[] change(int money, int price) {
         int[] coins = {10, 5, 2, 1};
-        int[] result = new int[coins.length + 1];
+        int[] result = new int[100];
         int size = 0;
         int change = money - price;
 
@@ -15,7 +15,6 @@ public class Machine {
                 change -= coin;
             }
         }
-
         return Arrays.copyOf(result, size);
     }
 }
