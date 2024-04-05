e50c35732e231e4b50ba10ef6d1fd46f394c58d5
AntonSadkov <antonsadkov96@gmail.com>
2024-04-05 11:55:28 +0300

    7.15 Перегрузить метод max для трёх чисел.

--- src/main/java/ru/condition/Max.java
+++ src/main/java/ru/condition/Max.java
@@ -5,9 +5,21 @@ public class Max {
         return left >= right ? left : right;
     }
 
+    public static int max(int first, int second, int third) {
+        return Math.max(first, Math.max(second, third));
+    }
+
+    public static int max(int first, int second, int third, int fourth) {
+        return Math.max(first, Math.max(second, Math.max(third, fourth)));
+    }
+
     public static void main(String[] args) {
         System.out.println(max(3, 2));
         System.out.println(max(2, 3));
         System.out.println(max(2, 2));
+        System.out.println(max(2, 2, 2));
+        System.out.println(max(2, 2, 3));
+        System.out.println(max(2, 2, 2, 2));
+        System.out.println(max(2, 2, 2, 3));
     }
 }
