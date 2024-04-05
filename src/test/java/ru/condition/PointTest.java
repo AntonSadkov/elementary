9a8378cb8d5ab725450b115460e9417116cbd582
AntonSadkov <antonsadkov96@gmail.com>
2024-04-05 12:12:05 +0300

    7.16 Растояние между точками в трёхмерном пространстве.

--- src/test/java/ru/condition/PointTest.java
+++ src/test/java/ru/condition/PointTest.java
@@ -16,7 +16,7 @@ class PointTest {
     }
 
     @Test
-    void when11To22Then2Dot4142() {
+    void when11To22Then1Dot4142() {
         Point a = new Point(1, 1);
         Point b = new Point(2, 2);
         double expected = 1.4142;
@@ -25,7 +25,7 @@ class PointTest {
     }
 
     @Test
-    void when22To33Then2() {
+    void when12To32Then2() {
         Point a = new Point(1, 2);
         Point b = new Point(3, 2);
         double expected = 2;
@@ -36,6 +36,33 @@ class PointTest {
     void when42To34Then2Dot23() {
         Point a = new Point(4, 2);
         Point b = new Point(3, 4);
+        double expected = 2.23;
+        assertThat(a.distance(b))
+                .isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when000To002Then2() {
+        Point a = new Point(0, 0, 0);
+        Point b = new Point(0, 0, 2);
+        double expected = 2;
+        assertThat(a.distance3d(b))
+                .isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when042To034Then1() {
+        Point a = new Point(0, 4, 2);
+        Point b = new Point(0, 3, 4);
+        double expected = 1;
+        assertThat(a.distance(b))
+                .isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void when420To340Then2Dot23() {
+        Point a = new Point(4, 2, 0);
+        Point b = new Point( 3, 4, 0);
         double expected = 2.23;
         assertThat(a.distance(b))
                 .isEqualTo(expected, withPrecision(0.01));
